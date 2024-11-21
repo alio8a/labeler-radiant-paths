dbd8138d290389df9efaa047bdb33f97990c8315
alio8a <alio8a>
2024-11-21 17:52:14 +0100

    subida de los paths

--- src/constants.ts
+++ src/constants.ts
@@ -4,10 +4,66 @@ export const DELETE = '3lbhagguhps27';
 export const LABEL_LIMIT = 1;
 export const LABELS: Label[] = [
   {
-    rkey: '3lbh36bdi7s2m',
+    rkey: '3lbhvhgoil22w',
     identifier: 'windrunner-path',
     locales: [
-      { lang: 'en', name: 'Windrunner 🍃', description: 'Windrunner'},  
+      { lang: 'en', name: 'Windrunner', description: 'Windrunner'},  
     ]
-  }
+  },
+  {
+    rkey: '3lbhvi5yvdk2w',
+    identifier: 'skybreaker-path',
+    locales: [
+      { lang: 'en', name: 'Skybreaker', description: 'Skybreaker'},  
+    ]
+  },
+  {
+    rkey: '3lbhvimmfcc2w',
+    identifier: 'dustbringer-path',
+    locales: [
+      { lang: 'en', name: 'Dustbringer', description: 'Dustbringer'},  
+    ]
+  },
+  {
+    rkey: '3lbhviwamlc2w',
+    identifier: 'edgedancer-path',
+    locales: [
+      { lang: 'en', name: 'Edgedancer', description: 'Edgedancer'},  
+    ]
+  },
+  {
+    rkey: '3lbhvj4gxb22w',
+    identifier: 'truthwatcher-path',
+    locales: [
+      { lang: 'en', name: 'Truthwatcher', description: 'Truthwatcher'},  
+    ]
+  },
+  {
+    rkey: '3lbhvjcdu6k2w',
+    identifier: 'lightweaver-path',
+    locales: [
+      { lang: 'en', name: 'Lightweaver', description: 'Lightweaver'},  
+    ]
+  },
+  {
+    rkey: '3lbhvjj5ynk2w',
+    identifier: 'elsecaller-path',
+    locales: [
+      { lang: 'en', name: 'Elsecaller', description: 'Elsecaller'},  
+    ]
+  },
+  {
+    rkey: '3lbhvjoqqes2w',
+    identifier: 'willshaper-path',
+    locales: [
+      { lang: 'en', name: 'Willshaper', description: 'Willshaper'},  
+    ]
+  },
+  {
+    rkey: '3lbhvjxopqk2w',
+    identifier: 'stoneward-path',
+    locales: [
+      { lang: 'en', name: 'Stoneward', description: 'Stoneward'},  
+    ]
+  },
 ];
